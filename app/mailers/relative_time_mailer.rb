d6f04f9c94923c5d31b455ef786ed00b5bc9805b
Prokop Simek <prokop.simek@applifting.cz>
2016-10-30 14:24:19 +0100

    fix up missing attrs

--- app/mailers/relative_time_mailer.rb
+++ app/mailers/relative_time_mailer.rb
@@ -1,10 +1,12 @@
 class RelativeTimeMailer < ApplicationMailer
-  def info_email(email)
+  def info_email(email, text)
 
       @server_time = DateTime.now
       Timecop.return do
         @real_time = DateTime.now
       end
+
+      @text = text
 
       mail(to: email, subject: 'Rubymeetup Prague - relative time example')
   end
