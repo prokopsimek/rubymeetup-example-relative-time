0507adb77863e201cfb194a5fab4f084c5437e1d
Prokop Simek <prokop.simek@applifting.cz>
2016-10-30 16:19:21 +0100

    fix saved time job

--- app/jobs/save_current_time_job.rb
+++ app/jobs/save_current_time_job.rb
@@ -2,9 +2,10 @@ class SaveCurrentTimeJob
   include Sidekiq::Worker
 
   def perform
-    saved_time =  DateTime.parse(Redis.new.get('current_testing_time'))
+    redis_time = Redis.new.get('current_testing_time')
+    saved_time = DateTime.parse(redis_time) if redis_time.present?
 
-    if saved_time < DateTime.now
+    if saved_time.present? && saved_time < DateTime.now
       Redis.new.set('current_testing_time', DateTime.now)
     end
   end
